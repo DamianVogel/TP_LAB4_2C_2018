a9b85a94b13a16be8a3a2a8217adcb7e16a6b206
Damian Vogel <rodolfovogel@gmail.com>
2018-10-18 22:54:07 -0300

    Cambios en el login

---
+++ src/app/componentes/registro/registro.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { Validators, FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { RegistroService } from '../../servicios/registro.service';
+import { Usuario } from '../../clases/usuario';
+
+function copiaClave(input: FormControl) {
+
+      if (input.root.get('clave') == null) {
+        alert("prueba");
+        return null;
+      }
+
+      const verificar = input.root.get('clave').value === input.value;
+      return verificar ? null : { mismaClave : true };
+  }
+
+@Component({
+  selector: 'app-registro',
+  templateUrl: './registro.component.html',
+  styleUrls: ['./registro.component.css']
+})
+
+export class RegistroComponent implements OnInit {
+
+  constructor(private builder: FormBuilder,
+              private _registro: RegistroService
+    ) { }
+
+  email = new FormControl('', [
+    Validators.required,
+    Validators.minLength(5),
+    Validators.email
+  ]);
+  
+  clave = new FormControl('', [
+    Validators.required,
+   Validators.minLength(4)
+  ]);
+  
+  copiaClave = new FormControl('', [
+    Validators.required,
+    copiaClave
+  ]);
+
+  registroForm: FormGroup = this.builder.group({
+    email: this.email,
+    clave: this.clave,
+    copiaClave: this.copiaClave,
+  });
+
+  ngOnInit() {
+  }
+
+  Registrar(){
+    
+    var usuario = new Usuario(this.registroForm.get('email').value, this.registroForm.get('clave').value);
+    console.log(usuario);
+
+    this._registro.Registro(usuario)
+    //.subscribe(data =>{})
+    ;
+
+    alert("Usuario Registrado");
+    console.log(this.registroForm.get('email').value); 
+  }
+
+}
